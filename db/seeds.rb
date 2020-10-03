707d358c4e15c09bc361db034805520ecf1d7caf
Stefan BAROLIN <stefan.barolin@gmail.com>
2020-10-03 15:04:17 +0200

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating ingredients in seeds'
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "mango juice")
+puts 'Ingredients created'
