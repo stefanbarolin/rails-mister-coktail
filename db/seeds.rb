75bb70c2dad1b74a507d9b31bdc20eb93ad6a7d8
Stefan BAROLIN <stefan.barolin@gmail.com>
2020-10-03 15:43:08 +0200

    Adding seeds for cocktail, ingredients, doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts 'Creating ingredients in seeds'
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+puts 'Creating Ingredients in seeds'
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
@@ -13,3 +17,20 @@ Ingredient.create(name: "rhum")
 Ingredient.create(name: "orange juice")
 Ingredient.create(name: "mango juice")
 puts 'Ingredients created'
+
+puts 'Creating Cocktails seeds'
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Pina Colada")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Moscow Mule")
+Cocktail.create(name: "Punch")
+puts 'Cocktails created'
+
+puts 'Creating Doses seeds for Cuba Libre'
+Dose.create(description: "1 zest", cocktail_id: 1, ingredient_id: 1)
+Dose.create(description: "2 rocks", cocktail_id: 1, ingredient_id: 2)
+Dose.create(description: "5 leaves", cocktail_id: 1, ingredient_id: 3)
+Dose.create(description: "3cl", cocktail_id: 1, ingredient_id: 4)
+Dose.create(description: "5 cl", cocktail_id: 1, ingredient_id: 5)
+puts 'Doses created'
