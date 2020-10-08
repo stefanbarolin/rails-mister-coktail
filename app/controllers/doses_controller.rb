9a3a2a452de500721d0585baa36858c3f01d4578
Stefan BAROLIN <stefan.barolin@gmail.com>
2020-10-08 20:55:36 +0200

    Add dotenv - Protect my secret data in .env file

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(set_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @doses = Dose.find(params[:id])
+    @doses.destroy
+  end
+
+  private
+
+  def set_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.destroy_all
-Cocktail.destroy_all
-Dose.destroy_all
+# Ingredient.destroy_all
+# Cocktail.destroy_all
+# Dose.destroy_all
 
-puts 'Creating Ingredients in seeds'
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "rhum")
-Ingredient.create(name: "orange juice")
-Ingredient.create(name: "mango juice")
-puts 'Ingredients created'
+# puts 'Creating Ingredients in seeds'
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "rhum")
+# Ingredient.create(name: "orange juice")
+# Ingredient.create(name: "mango juice")
+# puts 'Ingredients created'
 
-puts 'Creating Cocktails seeds'
-Cocktail.create(name: "Cuba Libre")
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Pina Colada")
-Cocktail.create(name: "Bloody Mary")
-Cocktail.create(name: "Moscow Mule")
-Cocktail.create(name: "Punch")
-puts 'Cocktails created'
+# puts 'Creating Cocktails seeds'
+# Cocktail.create(name: "Cuba Libre")
+# Cocktail.create(name: "Mojito")
+# Cocktail.create(name: "Pina Colada")
+# Cocktail.create(name: "Bloody Mary")
+# Cocktail.create(name: "Moscow Mule")
+# Cocktail.create(name: "Punch")
+# puts 'Cocktails created'
 
 puts 'Creating Doses seeds for Cuba Libre'
-Dose.create(description: "1 zest", cocktail_id: 1, ingredient_id: 1)
-Dose.create(description: "2 rocks", cocktail_id: 1, ingredient_id: 2)
-Dose.create(description: "5 leaves", cocktail_id: 1, ingredient_id: 3)
-Dose.create(description: "3cl", cocktail_id: 1, ingredient_id: 4)
-Dose.create(description: "5 cl", cocktail_id: 1, ingredient_id: 5)
+Dose.create(description: "1 zest", cocktail_id: 1, ingredient_id: 7)
+Dose.create(description: "2 rocks", cocktail_id: 1, ingredient_id: 8)
+Dose.create(description: "5 leaves", cocktail_id: 1, ingredient_id: 9)
+Dose.create(description: "3cl", cocktail_id: 1, ingredient_id: 10)
+Dose.create(description: "5 cl", cocktail_id: 1, ingredient_id: 11)
 puts 'Doses created'
